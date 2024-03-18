bd0805bfa405652da7c4f8cf86d832fe616f6384
SeongKwon_College <jungsam000@gmail.com>
2024-03-18 21:01:00 +0900

    0318_Mjc

---
+++ Chapter02/Ex_01/MainApp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_01
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요? \n반갑습니다!");
+        }
+    }
+}
